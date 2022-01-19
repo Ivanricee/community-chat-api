a21db454723feef215bf00569540ab1598580d85
Iván Bautista <ivanbautistag@gmail.com>
2022-01-18 20:03:46 -0600

    feature: entry points for servers & comments

---
+++ api/index.js
@@ -0,0 +1,353 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http"
+import express from "express"
+import cors from "cors"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const httpServer = http.createServer(app)
+
+// 1
+const typeDefs = gql`
+  # Server - channel - summary queryable fields.
+  type Summary {
+    _id: ID!
+    title: String
+    to: String
+    notification: String
+  }
+
+  type Channel {
+    _id: ID!
+    title: String
+    summary: [Summary]
+  }
+
+  type Server {
+    _id: ID!
+    title: String
+    img: String
+    alt: String
+    content: String
+    path: String
+    channels: [Channel]
+  }
+  # comments querysable fields
+  type Comment {
+    _id: ID
+    texto: String
+    img: String
+    url: String
+    react: [[String]]
+  }
+  type CComment {
+    _id_channel: ID
+    title: String
+    comments: [Comment]
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  # // Entry points
+  type Query {
+    servers: [Server]
+    findServer(_id: ID!): Server
+    findComment(_id_server: ID!, _id_channel: ID!): CComment
+  }
+`;
+// 2 Data source
+
+const servers = [
+  {
+    _id: "1",
+    title: "Ivanrice",
+    img: "https://res.cloudinary.com/ivanrice-c/image/upload/f_auto,q_auto:good/v1642023517/discord-clone/server/ivnrice_logo_grzmki.png",
+    alt: "server, ivanrice",
+    content: "8",
+    path: "/1/1",
+    channels: [
+      {
+        title: "- Welcome and rules",
+        _id: "1",
+        summary: [
+          {
+            _id: "1",
+            title: "extra-roles",
+            to: "/1/1",
+            notification: "0",
+          },
+        ],
+      },
+      {
+        title: "- important",
+        _id: "2",
+        summary: [
+          {
+            _id: "2",
+            title: "announcements",
+            to: "/1/2",
+            notification: "0",
+          },
+          {
+            _id: "3",
+            title: "calendar",
+            to: "/1/3",
+            notification: "9",
+          },
+        ],
+      },
+      {
+        title: "- resources",
+        _id: "3",
+        summary: [
+          {
+            _id: "4",
+            title: "therapy_harassment",
+            to: "/1/4",
+            notification: "0",
+          },
+          {
+            _id: "5",
+            title: "calendar",
+            to: "/1/5",
+            notification: "0",
+          },
+        ],
+      },
+      {
+        title: "- hg services",
+        _id: "4",
+        summary: [
+          {
+            _id: "6",
+            title: "coaching_questions",
+            to: "/1/6",
+            notification: "789",
+          },
+          {
+            _id: "7",
+            title: "gu_ide_help-desk",
+            to: "/1/7",
+            notification: "0",
+          },
+          {
+            _id: "8",
+            title: "gu_ide_suggestions",
+            to: "/1/8",
+            notification: "24",
+          },
+          {
+            _id: "9",
+            title: "gu_ide_bug-reports",
+            to: "/1/9",
+            notification: "0",
+          },
+        ],
+      },
+      {
+        title: "- hg content",
+        _id: "5",
+        summary: [
+          {
+            _id: "10",
+            title: "gu_ide_discussion",
+            to: "/1/10",
+            notification: "0",
+          },
+          {
+            _id: "11",
+            title: "content_discussion",
+            to: "/1/11",
+            notification: "0",
+          },
+        ],
+      },
+      {
+        title: "- growth",
+        _id: "6",
+        summary: [
+          {
+            _id: "12",
+            title: "improvement_general",
+            to: "/1/12",
+            notification: "0",
+          },
+          {
+            _id: "13",
+            title: "pogchamp",
+            to: "/1/13",
+            notification: "0",
+          },
+          {
+            _id: "14",
+            title: "life_purpose_and_dharma",
+            to: "/1/14",
+            notification: "957",
+          },
+          {
+            _id: "15",
+            title: "study_of_self",
+            to: "/1/15",
+            notification: "0",
+          },
+          {
+            _id: "16",
+            title: "careers",
+            to: "/1/16",
+            notification: "0",
+          },
+          {
+            _id: "17",
+            title: "student_life",
+            to: "/1/17",
+            notification: "2",
+          },
+          {
+            _id: "18",
+            title: "meditation_and_yoga",
+            to: "/1/18",
+            notification: "1",
+          },
+          {
+            _id: "19",
+            title: "physical_health",
+            to: "/1/19",
+            notification: "15",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id: "2",
+    title: "Healthy Gamer",
+    img: "https://res.cloudinary.com/ivanrice-c/image/upload/f_auto,q_auto:good/v1642023517/discord-clone/server/hg_logo_idpz31.png",
+    alt: "server, healthy gamer gg",
+    content: "61",
+    path: "/2/1",
+    channels: [
+      {
+        title: "- Welcome and rules",
+        _id: "1",
+        summary: [
+          {
+            _id: "2",
+            title: "announcements",
+            to: "/2/2",
+            notification: "0",
+          },
+          {
+            _id: "3",
+            title: "calendar",
+            to: "/2/3",
+            notification: "9",
+          },
+        ],
+      },
+    ],
+  },
+];
+const commentsChannel = [
+  {
+    _id_server: "1",
+    channels: [
+      {
+        _id_channel: "1",
+        title: "extra-roles",
+        comments: [
+          {
+            _id: "1",
+            texto: "Comentario uno",
+            img: "",
+            url: "",
+            react: ["emoji1", "emoji1", "emoji1", "emoji1", "emoji1"],
+          },
+          {
+            _id: "2",
+            texto: "comentario dos extra roles",
+            img: "",
+            url: "",
+            react: ["emoji1", "emoji1", "emoji1", "emoji1", "emoji1"],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    _id_server: "2",
+    channels: [
+      {
+        _id_channel: "1",
+        title: "announcements",
+        comments: [
+          {
+            _id: "1",
+            texto: "anuncio uno",
+            img: "",
+            url: "",
+            react: ["emoji1", "emoji1", "emoji1", "emoji1", "emoji1"],
+          },
+          {
+            _id: "2",
+            texto: "anuncio dos",
+            img: "",
+            url: "",
+            react: ["emoji1", "emoji1", "emoji1", "emoji1", "emoji1"],
+          },
+        ],
+      },
+    ],
+  },
+];
+// 3
+// Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+  Query: {
+    servers: () => servers,
+    findServer: (root, args)=>{
+      const { _id } = args;
+      const servidor = servers.find((server) => _id === server._id);
+      console.log("servidor ", servidor)
+      return servidor;
+    },
+    findComment: (root, args) => {
+      const { _id_server, _id_channel} = args;
+
+      const server = commentsChannel.find((server) => {
+        return server._id_server === _id_server;
+      });
+
+      const channels = server.channels.find((comment) => {
+        return comment._id_channel === _id_channel;
+      });
+
+      return channels;
+    }
+  },
+};
+
+// 4
+// The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const startApolloServer = async(app, httpServer) =>{
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  // The `listen` method launches a web server.
+  /*server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });*/
+}
+startApolloServer(app,httpServer)
+
+export default httpServer
+
